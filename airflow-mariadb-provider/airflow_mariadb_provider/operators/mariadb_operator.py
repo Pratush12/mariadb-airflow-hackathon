fb2c23086f4fb8d29628bb9112cafaf600c9fdf3
Pratush12 <pratushmaheshwari@gmail.com>
2025-09-26 02:17:23 +0530

    add mariadb connection hook

--- airflow-mariadb-provider/airflow_mariadb_provider/operators/mariadb_operator.py
+++ airflow-mariadb-provider/airflow_mariadb_provider/operators/mariadb_operator.py
@@ -1,33 +1,55 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Sequence
+from typing import Sequence
 
+from deprecated import deprecated
+
+from airflow.exceptions import AirflowProviderDeprecationWarning
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
-if TYPE_CHECKING:
-    from airflow.utils.context import Context
 
+@deprecated(
+    reason=(
+        "Please use `airflow.providers.common.sql.operators.sql.SQLExecuteQueryOperator`."
+        "Also, you can provide `hook_params={'schema': <database>}`."
+    ),
+    category=AirflowProviderDeprecationWarning,
+)
 class MariaDBOperator(SQLExecuteQueryOperator):
     """
-    Executes sql code against a MariaDB database.
+    Executes sql code in a MariaDB database
     """
 
-    template_fields: Sequence[str] = ('sql', 'params')
-    template_ext: Sequence[str] = ('.sql', '.json')
-    template_fields_renderers = {"sql": "sql"}
-    ui_color = '#e0f2f7'
+    template_fields: Sequence[str] = ("sql", "parameters")
+    template_fields_renderers = {
+        "sql": "mysql",
+        "parameters": "json",
+    }
+    template_ext: Sequence[str] = (".sql", ".json")
+    ui_color = "#ededed"
 
     def __init__(
-        self,
-        *,
-        mariadb_conn_id: str = "mariadb_default",
-        **kwargs,
-    ):
-        super().__init__(
-            conn_id=mariadb_conn_id,
-            **kwargs,
-        )
+        self, *, mariadb_conn_id: str = "mariadb_default", database: str | None = None, **kwargs
+    ) -> None:
+        if database is not None:
+            hook_params = kwargs.pop("hook_params", {})
+            kwargs["hook_params"] = {"schema": database, **hook_params}
 
-    def execute(self, context: Context):
-        self.log.info(f"Executing SQL query against MariaDB with conn_id: {self.conn_id}")
-        super().execute(context)
+        super().__init__(conn_id=mariadb_conn_id, **kwargs)
